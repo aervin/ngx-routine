837d631ba33df48b31a22c1cde961429ff3331cc
Aaron Ervin <aervin@uste3.com>
2018-06-10 10:27:29 -0400

    basic layout of test harness site

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display the default master detail text', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to ngx-routine-lib-app!');
+    expect(page.getMasterDetailText()).toEqual('routine-master-detail works!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { MatDividerModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxRoutineModule } from 'ngx-routine';
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
     NgxRoutineModule
   ],
   providers: [],
