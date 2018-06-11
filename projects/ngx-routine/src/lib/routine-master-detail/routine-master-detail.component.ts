b01b2a7addd95ade86e43d0328a5a2ad68269635
Aaron Ervin <aervin@uste3.com>
2018-06-11 14:12:25 -0400

    removes method from m/d class; doc updates in m/d class component

--- projects/ngx-routine/src/lib/routine-master-detail/routine-master-detail.component.ts
+++ projects/ngx-routine/src/lib/routine-master-detail/routine-master-detail.component.ts
@@ -11,46 +11,57 @@ export interface IdDisplayValuePair {
   styleUrls: ['./routine-master-detail.component.scss']
 })
 export class RoutineMasterDetailComponent implements OnInit {
-  /* A user-supplied array; the component's primary data source */
-  @Input() public dataSource: any[];
+
+  /**
+   * A user-supplied array; the component's primary data source
+   * @default undefined
+   * @memberof RoutineMasterDetailComponent
+   */
+  @Input() public dataSource: any[] | undefined;
 
   /**
    * When the user selects from the master list, we'll use this prop
    * to find the full object in the source array
+   * @default 'id'
+   * @memberof RoutineMasterDetailComponent
    */
   @Input() public discriminatorProperty = 'id';
 
-  /* A property we should use for display in the master list */
+  /**
+   * For every object in the dataSource array, the prop whose value
+   * should be used for display in the master list
+   * @default 'id'
+   * @memberof RoutineMasterDetailComponent
+   */
   @Input() public displayProperty = 'id';
+
 
   /**
    * A user-supplied ng-template ref; we pass this ref a data
    * context--this.detailData--in routine-master-detail.component.html
+   * @default undefined
+   * @memberof RoutineMasterDetailComponent
    */
   @Input() public detailTemplate: TemplateRef<any>;
 
-  public masterListDisplayData: string[] | number[] = [];
+  public masterListDisplayData: IdDisplayValuePair[] = [];
   public detailData: any | undefined;
 
   public ngOnInit(): void {
-    this.masterListDisplayData = initMasterListData(
-      this.dataSource,
-      this.discriminatorProperty,
-      this.displayProperty
-    );
+    /* Maps source data into something more manageable for the master list */
+    this.masterListDisplayData = this.dataSource.map((obj) => {
+      return { id: obj[this.discriminatorProperty], displayValue: obj[this.displayProperty] };
+    });
   }
 
-  /* Reassign values to the detail template's data context */
+  /**
+   * When a master list item is selected, find its full object value and
+   * update the detail template's data context
+   * @memberof RoutineMasterDetailComponent
+   */
   public handleMasterListItemSelection(item: IdDisplayValuePair): void {
     this.detailData = {
       $implicit: this.dataSource.find((data) => data[this.discriminatorProperty] === item.id)
     };
   }
-}
-
-/* Maps source data into something more manageable for the master list */
-function initMasterListData(data: any[], discriminator: string, displayProperty: string): any[] {
-  return data.map((obj) => {
-    return { id: obj[discriminator], displayValue: obj[displayProperty] };
-  });
 }
--- src/app/shared-components/shared-components.module.ts
+++ src/app/shared-components/shared-components.module.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatCardModule, MatDividerModule, MatTabsModule } from '@angular/material';
+import { MatCardModule, MatDividerModule, MatIconModule, MatTabsModule } from '@angular/material';
 import { NgxRoutineModule } from 'ngx-routine';
 import { ComponentDemoComponent } from './component-demo/component-demo.component';
 
 const COMPONENTS = [ComponentDemoComponent];
 
 @NgModule({
-  imports: [CommonModule, MatCardModule, MatDividerModule, MatTabsModule, NgxRoutineModule],
+  imports: [CommonModule, MatCardModule, MatDividerModule, MatIconModule, MatTabsModule, NgxRoutineModule],
   exports: COMPONENTS,
   declarations: COMPONENTS
 })
