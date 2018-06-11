08574c8eabe8d85081392a04f47bf5cc0321ea23
Aaron Ervin <aervin@uste3.com>
2018-06-11 10:16:37 -0400

    demo component accepting dynamic heading input

--- src/app/shared-components/component-demo/component-demo.component.ts
+++ src/app/shared-components/component-demo/component-demo.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-component-demo',
   templateUrl: './component-demo.component.html',
   styleUrls: ['./component-demo.component.css']
 })
-export class ComponentDemoComponent {}
+export class ComponentDemoComponent {
+  @Input() public heading: string;
+}
